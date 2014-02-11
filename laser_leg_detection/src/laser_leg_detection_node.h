20dca057e4b5dabd1f7941ffa601d5d519d3faa4
SeanBastable <bastabls2@gmail.com>
2014-02-11 00:37:26 +0000

    Added initial laser leg detection

---
+++ laser_leg_detection/src/laser_leg_detection_node.h
@@ -0,0 +1,17 @@
+/*
+ * LaserLegDetection.h
+ *
+ *  Created on: 27 Nov 2013
+ *      Author: sean
+ */
+
+#ifndef LASERLEGDETECTION_H_
+#define LASERLEGDETECTION_H_
+
+class LaserLegDetection {
+public:
+	LaserLegDetection();
+	virtual ~LaserLegDetection();
+};
+
+#endif /* LASERLEGDETECTION_H_ */
