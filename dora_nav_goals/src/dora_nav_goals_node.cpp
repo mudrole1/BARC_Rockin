1a51a820e3a77887d7bfdb123e2ef10786a13ab7
Manolis <manolis@dell-laptop.(none)>
2014-05-08 12:48:19 +0100

    Actula version of dora nav

--- dora_nav_goals/src/dora_nav_goals_node.cpp
+++ dora_nav_goals/src/dora_nav_goals_node.cpp
@@ -1,17 +1,22 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
-#include <std_msgs/Bool.h>
 #include <actionlib/client/simple_action_client.h>
+#include <std_msgs/Bool.h>
 
 // the custom msg containing the name and the coordinates of nav goals
 #include <dora_nav_goals/named_coordinates.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-ros::Publisher pub;
+
+// global variable to save from rewritting package with classes.
+// It stores if the nav goal was successful and if the callback was called
+std_msgs::Bool success, callBack;
 
 void NavRequestCallback(const dora_nav_goals::named_coordinatesConstPtr& msg)
 {
+    callBack.data = 1;
     ROS_INFO("I have got a Nav GOAL!!!");
+
 
     //tell the action client that we want to spin a thread by default
     MoveBaseClient ac("move_base", true);
@@ -25,19 +30,15 @@ void NavRequestCallback(const dora_nav_goals::named_coordinatesConstPtr& msg)
     ac.sendGoal(msg->goal);
     ac.waitForResult();
 
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
         ROS_INFO("Dora successfully moved to coordinates");
-
-        std_msgs::Bool response;
-        response.data = 1;
-        pub.publish(response);
+        success.data = 1;
     }
-    else {
+    else
+    {
         ROS_INFO("Dora failed to move properly for some reason");
-
-        std_msgs::Bool response;
-        response.data = 0;
-        pub.publish(response);
+        success.data = 0;
     }
 }
 
@@ -49,11 +50,21 @@ int main(int argc, char** argv){
 
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("/navigate/request", 1, NavRequestCallback);
+    ros::Subscriber sub = n.subscribe("/navigate/request", 10, NavRequestCallback);
+    ros::Publisher  pub = n.advertise<std_msgs::Bool>("/navigate/response", 10) ;
+    callBack.data = 0;
 
-    pub = n.advertise<std_msgs::Bool>("/navigate/response", 1000);
+    while (ros::ok())
+    {
+        if (callBack.data)
+        {
+            pub.publish(success);
+            callBack.data = 0;
+        }
 
-    ros::spin();
+       ros::spinOnce();
+    }
+
 
     return 0;
 }
