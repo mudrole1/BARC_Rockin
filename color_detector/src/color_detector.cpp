b58df3c8fb19187aecea8c4b708ab1b9ae20bad1
Joshua Smith <smithjoshua001@aol.co.uk>
2014-11-07 19:13:45 +0000

    Updated and added small calibration as well

--- color_detector/src/color_detector.cpp
+++ color_detector/src/color_detector.cpp
@@ -13,37 +13,62 @@
 #include "std_msgs/String.h"
 #include <std_msgs/Empty.h>
 #include <sensor_msgs/image_encodings.h>
+#include <iostream>
 
 static int N_SAMPLES, currentSample;
 static int delimanRedL, delimanRedH, delimanGreenL, delimanGreenH, delimanBlueL,
 		delimanBlueH;
 static int postmanRedL, postmanRedH, postmanGreenL, postmanGreenH, postmanBlueL,
 		postmanBlueH;
+static int threshold;
 static ros::Publisher detectedPerson;
 static bool startDetecting;
+static bool initFlag = true;
+
+
+
 
 void init(ros::NodeHandle n) {
-	delimanRedL = 100;
-	delimanGreenL = 100;
-	delimanBlueL = 100;
-	delimanRedH = 100;
-	delimanGreenH = 100;
-	delimanBlueH = 100;
+	delimanRedH = 136;
+	delimanGreenH = 166;
+	delimanBlueH = 167;
+	delimanRedL = 51;
+	delimanGreenL = 87;
+	delimanBlueL = 80;
+
 	postmanRedL = 100;
 	postmanGreenL = 100;
 	postmanBlueL = 100;
 	postmanRedH = 100;
 	postmanGreenH = 100;
 	postmanBlueH = 100;
-	ros::Publisher detectedPerson;
+
+	threshold=1.8e+07;
+
 	detectedPerson = n.advertise<std_msgs::String>("/recognition/response", 1);
 	startDetecting = false;
 	N_SAMPLES = 10;
 	currentSample = 0;
+	cv::namedWindow("deliman");
+	cv::namedWindow("normal");
+}
+
+void cameraInit(ros::NodeHandle n) {
+	init(n);
+	cv::namedWindow("Calibrate", CV_WINDOW_AUTOSIZE);
+	cv::createTrackbar("LowR", "Calibrate", &delimanRedL, 255); //Hue (0 - 179)
+	cv::createTrackbar("HighR", "Calibrate", &delimanRedH, 255);
+
+	cv::createTrackbar("LowG", "Calibrate", &delimanGreenL, 255); //Saturation (0 - 255)
+	cv::createTrackbar("HighG", "Calibrate", &delimanGreenH, 255);
+
+	cv::createTrackbar("LowB", "Calibrate", &delimanBlueL, 255); //Value (0 - 255)
+	cv::createTrackbar("HighB", "Calibrate", &delimanBlueH, 255);
+
 }
 
 void detect(const sensor_msgs::ImageConstPtr& img) {
-	if (startDetecting) {
+	if (initFlag) {
 		cv_bridge::CvImagePtr cv_ptr;
 		try {
 			cv_ptr = cv_bridge::toCvCopy(img,
@@ -52,7 +77,6 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 			ROS_ERROR("cv_bridge exception: %s", e.what());
 			return;
 		}
-
 		cv::Mat thresholdedImg;
 		cv::inRange(cv_ptr->image,
 				cv::Scalar(delimanBlueL, delimanGreenL, delimanRedL),
@@ -69,11 +93,44 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
 		cv::erode(thresholdedImg, thresholdedImg,
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+		cv::imshow("Calibrate", thresholdedImg);
+		cv::waitKey(1);
+	}
+	if (startDetecting) {
+		cv_bridge::CvImagePtr cv_ptr;
+		try {
+			cv_ptr = cv_bridge::toCvCopy(img,
+					sensor_msgs::image_encodings::BGR8);
+		} catch (cv_bridge::Exception& e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+		std::cout << "go\n";
+		flush(std::cout);
+		cv::Mat thresholdedImg;
+		cv::inRange(cv_ptr->image,
+				cv::Scalar(delimanBlueL, delimanGreenL, delimanRedL),
+				cv::Scalar(delimanBlueH, delimanGreenH, delimanRedH),
+				thresholdedImg);
+		//morphological opening (remove small objects from the foreground)
+		cv::erode(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+		cv::dilate(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
 
-		cv::Moments moment= cv::moments(thresholdedImg);
+		//morphological closing (fill small holes in the foreground)
+		cv::dilate(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+		cv::erode(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+		cv::imshow("normal", cv_ptr->image);
+		cv::imshow("deliman", thresholdedImg);
+		cv::waitKey(5000);
+		cv::Moments moment = cv::moments(thresholdedImg);
 		double delimanArea = moment.m00;
-
-		if (delimanArea > 10000) {
+		std::cout << delimanArea << " deliman area\n";
+		flush(std::cout);
+		if (delimanArea > threshold) {
 			std_msgs::String person;
 			person.data = "Deliman";
 			detectedPerson.publish(person);
@@ -81,7 +138,6 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 			currentSample = 0;
 			return;
 		}
-
 
 		cv::inRange(cv_ptr->image,
 				cv::Scalar(postmanBlueL, postmanGreenL, postmanRedL),
@@ -102,7 +158,7 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 		moment = cv::moments(thresholdedImg);
 		double postmanArea = moment.m00;
 
-		if (postmanArea > 10000) {
+		if (postmanArea > threshold) {
 			std_msgs::String person;
 			person.data = "Postman";
 			detectedPerson.publish(person);
@@ -115,6 +171,7 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 			person.data = "Unknown";
 			detectedPerson.publish(person);
 			currentSample = 0;
+			startDetecting = false;
 		} else {
 			currentSample++;
 		}
@@ -122,15 +179,16 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 	}
 }
 
-void start(std_msgs::EmptyConstPtr) {
+void start(const std_msgs::EmptyConstPtr& empty) {
 	startDetecting = true;
+	initFlag = false;
 }
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "color_detector");
 	ros::NodeHandle n;
-	init(n);
-	ros::Subscriber node_sub = n.subscribe("/camera/image_raw", 1, detect);
+	cameraInit(n);
+	ros::Subscriber node_sub = n.subscribe("/usb_cam/image_raw", 1, detect);
 	ros::Subscriber startDetect = n.subscribe("/color_detect/start", 1, start);
 	ros::spin();
 	return 0;
