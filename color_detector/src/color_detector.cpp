1979abeafaf5847d6021dd7aabc117df042baa7b
Joshua Smith <smithjoshua001@aol.co.uk>
2014-11-10 15:10:17 +0000

    Color detection with file calibration and start calibration

--- color_detector/src/color_detector.cpp
+++ color_detector/src/color_detector.cpp
@@ -14,61 +14,98 @@
 #include <std_msgs/Empty.h>
 #include <sensor_msgs/image_encodings.h>
 #include <iostream>
+#include <fstream>
+#include <string>
 
 static int N_SAMPLES, currentSample;
-static int delimanRedL, delimanRedH, delimanGreenL, delimanGreenH, delimanBlueL,
-		delimanBlueH;
-static int postmanRedL, postmanRedH, postmanGreenL, postmanGreenH, postmanBlueL,
-		postmanBlueH;
+//static int delimanRedL, delimanRedH, delimanGreenL, delimanGreenH, delimanBlueL,
+//		delimanBlueH;
+static int deliman[6], postman[6];
+//static int postmanRedL, postmanRedH, postmanGreenL, postmanGreenH, postmanBlueL,
+//		postmanBlueH;
 static int threshold;
 static ros::Publisher detectedPerson;
 static bool startDetecting;
 static bool initFlag = true;
-
-
-
+static std::string calibFile;
 
 void init(ros::NodeHandle n) {
-	delimanRedH = 136;
-	delimanGreenH = 166;
-	delimanBlueH = 167;
-	delimanRedL = 51;
-	delimanGreenL = 87;
-	delimanBlueL = 80;
 
-	postmanRedL = 100;
-	postmanGreenL = 100;
-	postmanBlueL = 100;
-	postmanRedH = 100;
-	postmanGreenH = 100;
-	postmanBlueH = 100;
+	n.param<std::string>("/color_detector/calibFile", calibFile, "");
+	ROS_INFO("Got param: %s", calibFile.c_str());
+	std::ifstream inFile(calibFile.c_str());
+	std::string lineIn;
+	while (std::getline(inFile, lineIn)) {
+		ROS_INFO("READ LINE: %s", lineIn.c_str());
+		if (std::strcmp(lineIn.c_str(), "Deliman") == 0) {
+			std::getline(inFile, lineIn);
+			int i = 0;
+			while (std::strcmp(lineIn.c_str(), "Stop")) {
+				ROS_INFO("READ LINE: %s", lineIn.c_str());
+				deliman[i] = atoi(lineIn.c_str());
+				i++;
+				std::getline(inFile, lineIn);
+			}
+		} else {
+			std::getline(inFile, lineIn);
+			int i = 0;
+			while (std::strcmp(lineIn.c_str(), "Stop")) {
+				postman[i] = atoi(lineIn.c_str());
+				i++;
+				std::getline(inFile, lineIn);
+			}
+		}
+	}
+	inFile.close();
+//	delimanRedH = deliman[0];
+//	delimanGreenH = deliman[1];
+//	delimanBlueH = deliman[2];
+//	delimanRedL = deliman[3];
+//	delimanGreenL = deliman[4];
+//	delimanBlueL = deliman[5];
 
-	threshold=1.8e+07;
+//	postmanRedL = 100;
+//	postmanGreenL = 100;
+//	postmanBlueL = 100;
+//	postmanRedH = 100;
+//	postmanGreenH = 100;
+//	postmanBlueH = 100;
+
+	threshold = 1.8e+07;
 
 	detectedPerson = n.advertise<std_msgs::String>("/recognition/response", 1);
 	startDetecting = false;
 	N_SAMPLES = 10;
 	currentSample = 0;
-	cv::namedWindow("deliman");
-	cv::namedWindow("normal");
 }
 
 void cameraInit(ros::NodeHandle n) {
 	init(n);
-	cv::namedWindow("Calibrate", CV_WINDOW_AUTOSIZE);
-	cv::createTrackbar("LowR", "Calibrate", &delimanRedL, 255); //Hue (0 - 179)
-	cv::createTrackbar("HighR", "Calibrate", &delimanRedH, 255);
+	cv::namedWindow("Calibrate Deliman", CV_WINDOW_AUTOSIZE);
+	cv::createTrackbar("LowR", "Calibrate Deliman", &deliman[2], 255); //Hue (0 - 179)
+	cv::createTrackbar("HighR", "Calibrate Deliman", &deliman[5], 255);
 
-	cv::createTrackbar("LowG", "Calibrate", &delimanGreenL, 255); //Saturation (0 - 255)
-	cv::createTrackbar("HighG", "Calibrate", &delimanGreenH, 255);
+	cv::createTrackbar("LowG", "Calibrate Deliman", &deliman[1], 255); //Saturation (0 - 255)
+	cv::createTrackbar("HighG", "Calibrate Deliman", &deliman[4], 255);
 
-	cv::createTrackbar("LowB", "Calibrate", &delimanBlueL, 255); //Value (0 - 255)
-	cv::createTrackbar("HighB", "Calibrate", &delimanBlueH, 255);
+	cv::createTrackbar("LowB", "Calibrate Deliman", &deliman[0], 255); //Value (0 - 255)
+	cv::createTrackbar("HighB", "Calibrate Deliman", &deliman[3], 255);
+
+	cv::namedWindow("Calibrate Postman", CV_WINDOW_AUTOSIZE);
+	cv::createTrackbar("LowR", "Calibrate Postman", &postman[2], 255); //Hue (0 - 179)
+	cv::createTrackbar("HighR", "Calibrate Postman", &postman[5], 255);
+
+	cv::createTrackbar("LowG", "Calibrate Postman", &postman[1], 255); //Saturation (0 - 255)
+	cv::createTrackbar("HighG", "Calibrate Postman", &postman[4], 255);
+
+	cv::createTrackbar("LowB", "Calibrate Postman", &postman[0], 255); //Value (0 - 255)
+	cv::createTrackbar("HighB", "Calibrate Postman", &postman[1], 255);
 
 }
 
 void detect(const sensor_msgs::ImageConstPtr& img) {
 	if (initFlag) {
+
 		cv_bridge::CvImagePtr cv_ptr;
 		try {
 			cv_ptr = cv_bridge::toCvCopy(img,
@@ -77,11 +114,16 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 			ROS_ERROR("cv_bridge exception: %s", e.what());
 			return;
 		}
+
+		/******************** DELIMAN *******************************/
 		cv::Mat thresholdedImg;
+//		cv::inRange(cv_ptr->image,
+//				cv::Scalar(delimanBlueL, delimanGreenL, delimanRedL),
+//				cv::Scalar(delimanBlueH, delimanGreenH, delimanRedH),
+//				thresholdedImg);
 		cv::inRange(cv_ptr->image,
-				cv::Scalar(delimanBlueL, delimanGreenL, delimanRedL),
-				cv::Scalar(delimanBlueH, delimanGreenH, delimanRedH),
-				thresholdedImg);
+				cv::Scalar(deliman[0], deliman[1], deliman[2]),
+				cv::Scalar(deliman[3], deliman[4], deliman[5]), thresholdedImg);
 		//morphological opening (remove small objects from the foreground)
 		cv::erode(thresholdedImg, thresholdedImg,
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
@@ -93,7 +135,25 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
 		cv::erode(thresholdedImg, thresholdedImg,
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-		cv::imshow("Calibrate", thresholdedImg);
+		cv::imshow("Calibrate Deliman", thresholdedImg);
+
+		/******************** POSTMAN *******************************/
+		cv::inRange(cv_ptr->image,
+				cv::Scalar(postman[0], postman[1], postman[2]),
+				cv::Scalar(postman[3], postman[4], postman[5]), thresholdedImg);
+		//morphological opening (remove small objects from the foreground)
+		cv::erode(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+		cv::dilate(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+
+		//morphological closing (fill small holes in the foreground)
+		cv::dilate(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+		cv::erode(thresholdedImg, thresholdedImg,
+				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+		cv::imshow("Calibrate Postman", thresholdedImg);
+
 		cv::waitKey(1);
 	}
 	if (startDetecting) {
@@ -109,9 +169,8 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 		flush(std::cout);
 		cv::Mat thresholdedImg;
 		cv::inRange(cv_ptr->image,
-				cv::Scalar(delimanBlueL, delimanGreenL, delimanRedL),
-				cv::Scalar(delimanBlueH, delimanGreenH, delimanRedH),
-				thresholdedImg);
+				cv::Scalar(deliman[0], deliman[1], deliman[2]),
+				cv::Scalar(deliman[3], deliman[4], deliman[5]), thresholdedImg);
 		//morphological opening (remove small objects from the foreground)
 		cv::erode(thresholdedImg, thresholdedImg,
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
@@ -123,9 +182,6 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
 		cv::erode(thresholdedImg, thresholdedImg,
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-		cv::imshow("normal", cv_ptr->image);
-		cv::imshow("deliman", thresholdedImg);
-		cv::waitKey(1000);
 		cv::Moments moment = cv::moments(thresholdedImg);
 		double delimanArea = moment.m00;
 		std::cout << delimanArea << " deliman area\n";
@@ -139,10 +195,10 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 			return;
 		}
 
+		/******************* POSTMAN ********************************************/
 		cv::inRange(cv_ptr->image,
-				cv::Scalar(postmanBlueL, postmanGreenL, postmanRedL),
-				cv::Scalar(postmanBlueH, postmanGreenH, postmanRedH),
-				thresholdedImg);
+				cv::Scalar(postman[0], postman[1], postman[2]),
+				cv::Scalar(postman[3], postman[4], postman[5]), thresholdedImg);
 		//morphological opening (remove small objects from the foreground)
 		cv::erode(thresholdedImg, thresholdedImg,
 				cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
@@ -174,6 +230,7 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 			startDetecting = false;
 		} else {
 			currentSample++;
+			sleep(2);
 		}
 
 	}
@@ -182,13 +239,28 @@ void detect(const sensor_msgs::ImageConstPtr& img) {
 void start(const std_msgs::EmptyConstPtr& empty) {
 	startDetecting = true;
 	initFlag = false;
+	if (std::strcmp(calibFile.c_str(), "") != 0) {
+		std::ofstream outFile(calibFile.c_str());
+		outFile << "Deliman\n";
+		int i;
+		for (i = 0; i < 6; i++) {
+			outFile << deliman[i] << "\n";
+		}
+		outFile << "Stop\n";
+		outFile << "Postman\n";
+		for (i = 0; i < 6; i++) {
+			outFile << postman[i] << "\n";
+		}
+		outFile <<"Stop\n";
+	}
+
 }
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "color_detector");
 	ros::NodeHandle n;
 	cameraInit(n);
-	ros::Subscriber node_sub = n.subscribe("/camera/image_raw", 1, detect);
+	ros::Subscriber node_sub = n.subscribe("/usb_cam/image_raw", 1, detect);
 	ros::Subscriber startDetect = n.subscribe("/color_detect/start", 1, start);
 	ros::spin();
 	return 0;
